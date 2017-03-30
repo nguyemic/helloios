55d611004e0ef3263333e86ce3ce167914bbaa76
Michael Nguyen <michaelnguyen@Michaels-Mac.local>
2017-03-29 18:22:18 -0700

    Initial Commit

---
+++ Hello-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello-iOS
+//
+//  Created by Michael Nguyen on 3/29/17.
+//  Copyright © 2017 Michael Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
